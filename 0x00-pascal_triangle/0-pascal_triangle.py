63d2f9d1c9d68d070cf36aee9411cc87f9b1f9ab
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-12 03:36:52 +0200

    Fix Pascal's Triangle implementation and remove unused code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,6 @@
+import math 
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -7,6 +10,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
+            row.append(math.comb(i, j))
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
