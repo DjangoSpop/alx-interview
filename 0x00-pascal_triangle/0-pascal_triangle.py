e4b5475476e443f4778ed466135e723b5610ae82
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-12 03:43:06 +0200

    Refactor Pascal's Triangle implementation and optimize code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,15 @@
 import math 
 
-
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(math.comb(i, j))
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(math.factorial(i) // (math.factorial(j) * math.factorial(i - j)))
         triangle.append(row)
-    
     return triangle
