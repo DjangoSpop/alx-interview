800623438dc096a62292072dcbce6d1014df321d
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-20 18:07:51 +0200

    we can

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,18 @@
-#!/usr/bin/python3
 def canUnlockAll(boxes):
-    """Determine if all the boxes can be opened."""
-    if not boxes:
-        return False
-    if not boxes[0]:
-        return False
-    keys = [0]
-    for key in keys:
-        for box in boxes[key]:
-            if box not in keys and box < len(boxes):
-                keys.append(box)
-    return len(keys) == len(boxes)
+    
+    visited = [False] * len(boxes)  # Keep track of visited boxes
+    visited[0] = True  # Mark the first box as visited
+    stack = [0]  # Start with the first box
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    while stack:
+        box = stack.pop()  # Get the top box from the stack
+        for key in boxes[box]:
+            if not visited[key]:
+                visited[key] = True  # Mark the box as visited
+                stack.append(key)  # Add the box to the stack
+
+    return all(visited)  # Return True if all boxes are visited, else False
