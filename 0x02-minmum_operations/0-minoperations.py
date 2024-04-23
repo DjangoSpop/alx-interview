cb470436ccd82c6f219f8bfdb6e88ce00d628e2a
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-23 03:00:40 +0200

    we can do it

---
+++ 0x02-minmum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+def minOperations(n):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+    
+    return operations
