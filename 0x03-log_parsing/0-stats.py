a15dfd76772845371db9d1518ad08137985a4cf1
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-04 10:49:47 +0300

    do it

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-"""
-Log parsing
+"""Log Parsing
+Write a script that reads stdin line by line and computes metrics:
 """
 import sys
-import os
-
-if not os.path.isatty(0):
-    sys.stdin.seek(0)
 
 
-if __name__ == '__main__':
+total_file_size = 0
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+obj = dict.fromkeys(status, 0)
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict) -> None:
-        """
-        Print the statistics from the given dictionary.
+def printLogStat():
+    """Print log statistics"""
+    print("File size: {}".format(total_file_size))
+    for key, value in sorted(obj.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
-        Args:
-            stats (dict): A dictionary containing the statistics.
 
-        Returns:
-            None
-        """
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
+if __name__ == "__main__":
+    count = 0
     try:
         for line in sys.stdin:
+            line = line.split()
             count += 1
-            data = line.split()
             try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
+                total_file_size += int(line[-1])
+
+                if line[-2] in status:
+                    obj[line[-2]] += 1
+
+            except (IndexError, ValueError):
                 pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
+
             if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+                printLogStat()
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+        pass
+    finally:
+        printLogStat()
