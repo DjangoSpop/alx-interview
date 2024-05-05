545b6b50455f556b824ebe127eb22b14a4cd2f28
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-05 11:58:17 +0300

    weeeee

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,37 +3,43 @@
 
 import sys
 import re
-
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 counter = 0
-ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-date_pattern = r'\d{4}-\d{2}-\d{2}'
-time_pattern = r'\d{2}:\d{2}:\d{2}'
-request_pattern = r'\"[A-Z]{3,7} \/.*?\"'
-status_pattern = r'\d{3}'
-size_pattern = r'\d+'
-
-for line in sys.stdin:
-    match = re.match(f'({ip_pattern}) - \[({date_pattern}) ({time_pattern})\] {request_pattern} ({status_pattern}) ({size_pattern})', line)
-    if match:
-        code = match.group(5)
-        size = int(match.group(6))
-        if code in cache:
-            cache[code] += 1
-        total_size += size
-        counter += 1
-
-    if counter == 10:
-        counter = 0
-        print('Total file size:', total_size)
-        for key, value in sorted(cache.items()):
-            if value != 0:
-                print(key + ':', value)
+pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$'
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
+        line_list = re.findall(pattern, line)
+        if line_list:
+            size = int(line_list[0][3])
+            code = line_list[0][2]
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            print('Total file size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except KeyboardInterrupt:
+    print('Total file size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
+
+except Exception as err:
+    print('Error: {}'.format(err))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
+        if line_list[7].isdigit():
+            size = int(line_list[7])
+        else:
+            continue
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
@@ -43,7 +49,6 @@ try:
             counter += 1
 
         if counter == 10:
-            counter = 0
             print('Total file size: {}'.format(total_size))
             for key, value in sorted(cache.items()):
                 if value != 0:
