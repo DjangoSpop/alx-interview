d26d93bebb6f30f39b2cb036f0a1c5bff0858ae8
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-24 02:56:39 +0300

    Refactor Star Wars character retrieval code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -14,20 +14,15 @@ request(apiUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const movie = JSON.parse(body);
     const characters = movie.characters;
-
-    printCharacters(characters);
+    characters.forEach(function (url) {
+      request(url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
   } else {
     console.error(`Error: ${error || response.statusCode}`);
   }
 });
-
-function printCharacters (urls) {
-  urls.forEach(function (url) {
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const character = JSON.parse(body);
-        console.log(character.name);
-      }
-    });
-  });
-}
