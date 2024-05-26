1719eb92e767daebf018d0a9532b1e5205484746
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-26 18:37:52 +0300

    weeee

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
-request(url, function(error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    else {
-        let films = JSON.parse(body);
-        let characters = films.characters;
-        for (let index = 0; index < characters.length; index++) {
-            request(characters[index], function(error, response, body) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    let character = JSON.parse(body);
-                    console.log(character.name);
-                }
-            });
-        }
-    }
-});
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
+
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
+}
+
+starwarsCharacters(filmID);
