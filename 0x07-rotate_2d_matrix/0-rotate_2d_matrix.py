acde407e5ae696c38893835fffd208ccf944fcfd
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-29 16:31:33 +0300

    Refactor rotate_2d_matrix function

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""module to rotate a marix of x and x."""
+
+
 def rotate_2d_matrix(matrix):
-    """_summary_
- Args:
- matrix (_type_): _description_
-    """
+    """_summary_Args:matrix (_type_): _description_."""
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
