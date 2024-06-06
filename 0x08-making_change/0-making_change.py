b7bef1b2b0c50bc82dd17fcb312048cdb783d739
BehoSpop <behodjango@gmail.com>
2024-06-07 01:58:13 +0300

    23

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""Change making module.
-"""
+"""Change making module."""
 
 
 def makeChange(coins, total):
+    """_summary.
+
+    Args:
+        coins (_type_): _description_
+        total (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
     if total <= 0:
         return 0
 
-    # Create an array to store the minimum number of coins required to reach each value
+    # Create an array to store the minimum number
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
 
