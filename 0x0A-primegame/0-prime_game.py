8d418596a9ebb5e8c071ad7b85d20a345cfb497d
BehoSpop <behodjango@gmail.com>
2024-06-18 01:11:05 +0200

    we

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def isWinner(x, nums):
     def is_prime(num):
         if num < 2:
@@ -8,26 +7,28 @@ def isWinner(x, nums):
                 return False
         return True
 
+    def memoize(func):
+        cache = {}
+
+        def wrapper(n):
+            if n not in cache:
+                cache[n] = func(n)
+            return cache[n]
+        return wrapper
+
+    @memoize
     def play_game(n):
-        nums = set(range(1, n + 1))
-        player = 0
-        while True:
-            found_prime = False
-            for num in nums:
-                if is_prime(num):
-                    found_prime = True
-                    nums.remove(num)
-                    for multiple in range(num * 2, n + 1, num):
-                        nums.discard(multiple)
-                    break
-            if not found_prime:
-                return player
-            player = 1 - player
+        if n == 1:
+            return False
+        for i in range(2, n + 1):
+            if is_prime(i) and not play_game(n - i):
+                return True
+        return False
 
     maria_wins = ben_wins = 0
     for n in nums:
         winner = play_game(n)
-        if winner == 0:
+        if winner:
             maria_wins += 1
         else:
             ben_wins += 1
